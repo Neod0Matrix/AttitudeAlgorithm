1bbd6ac089b90f291281796c6f431fe0536621c1
Neod0Matrix <1054465075@qq.com>
2018-07-01 18:57:44 +0800

    Framework Update

--- EmbeddedBreakerCore/Modules/link.c
+++ EmbeddedBreakerCore/Modules/link.c
@@ -197,5 +197,11 @@ void Modules_StatusReqHandler (void)
 	myfree(aa_dtbuf);
 }
 
+//模块插入到exti.c的PB8外部中断函数EXTI9_5_IRQHandler内，触发外部中断打断
+void Modules_EXTI8_IRQHandler (void)
+{
+	//通常来说可以在工控系统内紧急停止电机运转
+}
+
 //====================================================================================================
 //code by </MATRIX>@Neod Anderjon
--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Copyright
 Update
 ===============
 	
-	最后更新时间: 2018/04/17pm2122
+	最后更新时间: 2018/07/01pm1851
 	详细更新内容见softlog.txt
 
 Project Manual
